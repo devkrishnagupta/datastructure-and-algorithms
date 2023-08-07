d8d26a329c3f6936d2024d676a51633a926dd023
Dev Krishna Gupta <dev.trisect@gmail.com>
2023-08-08 00:40:15 +0530

    solve 74. Search a 2D Matrix using BINARY SEARCH TREE

--- leetcode/74. Search a 2D Matrix/74. Search a 2D Matrix.java
+++ leetcode/74. Search a 2D Matrix/74. Search a 2D Matrix.java
@@ -1,3 +1,4 @@
+//Approach-1 (Using BINARY SEARCH)
 class Solution {
     public boolean searchMatrix(int[][] matrix, int target) {
         int n = matrix.length,m = matrix[0].length;
@@ -12,5 +13,24 @@ class Solution {
                 h = mid - 1;
         }
         return false;   
+    }
+}
+
+//Approach-2 (Using BINARY SEARCH TREE)
+class Solution {
+    public boolean searchMatrix(int[][] matrix, int target) {
+        int m=matrix.length;
+        int n=matrix[0].length;
+        int i=0, j=n-1;
+        while(i<m && j>=0){
+            if(matrix[i][j]==target){
+                return true;
+            }
+            if(matrix[i][j]<target)
+                i++;
+            else
+                j--;
+        }
+        return false;
     }
 }
