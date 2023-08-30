490d5b61a2cd1d36ec4ff85bad315cff5046bd7b
Dev Krishna Gupta <dev.trisect@gmail.com>
2023-08-30 21:40:30 +0530

    Delete a Node in Single Linked List

--- geeksforgeeks/Delete nodes having greater value on right/Delete nodes having greater value on right.java
+++ geeksforgeeks/Delete nodes having greater value on right/Delete nodes having greater value on right.java
@@ -143,6 +143,39 @@ class Solution
     }
 }
   
-//Approach-3
+//Approach-3 (Using reversing linked list)
 //Time n
 //Space 1
+class Solution
+{
+    Node compute(Node head)
+    {
+        // your code here
+        if(head.next==null)return head;
+        Node new_head=rev(head);
+        Node curr=new_head;
+        Node temp=null;
+        Node maxNode=curr;
+        while(curr.next!=null){
+            if(curr.next.data<curr.data){
+                temp=curr.next;
+                curr.next=temp.next;
+            }else{
+                curr=curr.next;
+                maxNode=curr;
+            }
+        }
+        return rev(new_head);
+    }
+    
+    Node rev(Node head){
+        Node prev=null,curr=head,next=null;
+        while(curr!=null){
+            next=curr.next;
+            curr.next=prev;
+            prev=curr;
+            curr=next;
+        }
+        return prev;
+    }
+}
