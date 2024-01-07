b6a8802d2b9acc2dc75c72785b0bd3f0a30fb1de
Dev Krishna Gupta <dev.trisect@gmail.com>
2024-01-07 20:24:38 +0530

    Arithmetic Slices II - Subsequence

---
+++ dynamicProgramming/446. Arithmetic Slices II - Subsequence/446. Arithmetic Slices II - Subsequence.java
@@ -0,0 +1,24 @@
+//T.C : O(n^2)
+//S.C : O(n^2) - because of the 2D array mp, where each row mp[i] represents a different index in the input vector nums
+public class Solution {
+    public int numberOfArithmeticSlices(int[] nums) {
+        int n = nums.length;
+        int result = 0;
+        Map<Long, Integer>[] mp = new HashMap[n];
+
+        for (int i = 0; i < n; i++) {
+            mp[i] = new HashMap<>();
+            for (int j = 0; j < i; j++) {
+                long diff = (long) nums[i] - nums[j];
+                int count_at_j = mp[j].getOrDefault(diff, 0);
+
+                result += count_at_j;
+                
+                // Increment the count at index i
+                mp[i].put(diff, mp[i].getOrDefault(diff, 0) + count_at_j + 1);
+            }
+        }
+        return result;
+    }
+
+}
