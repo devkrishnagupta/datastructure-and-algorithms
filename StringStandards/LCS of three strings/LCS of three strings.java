72c3671950a8f070e864b3aedd604c2459dd3ee7
Dev Krishna Gupta <dev.trisect@gmail.com>
2024-01-31 23:59:20 +0530

    Insert and Search in a Trie

--- StringStandards/LCS of three strings/LCS of three strings.java
+++ StringStandards/LCS of three strings/LCS of three strings.java
@@ -26,7 +26,7 @@ class GFG{
 
 //User function Template for Java
 
-// Approach-1
+// Approach-1 (Dynamic + Recursion)
 // T.C : O(n1 * n2 * n3)
 // S.C : O(n1 * n2 * n3)
 class Solution 
@@ -66,5 +66,33 @@ class Solution
         int not_taken_k = solve(A, B, C, i, j, k + 1);
         
         return t[i][j][k] = Math.max(Math.max(not_taken_i, not_taken_j), not_taken_k);
+    }
+}
+
+//Approach-2 (Bootom-up dynamic programming)
+// T.C : O(n1 * n2 * n3)
+// S.C : O(n1 * n2 * n3)
+class Solution 
+{ 
+    int LCSof3(String A, String B, String C, int n1, int n2, int n3) 
+    { 
+        // code here
+        int[][][] dp = new int[n1 + 1][n2 + 1][n3 + 1];
+        
+        for(int i = 0; i <= n1; i++){
+            for(int j = 0; j <= n2; j++){
+                for(int k = 0; k <= n3; k++){
+                    if(i == 0 || j == 0 || k == 0){
+                        dp[i][j][k] = 0;
+                    }else if(A.charAt(i - 1) == B.charAt(j - 1) && B.charAt(j - 1) == C.charAt(k - 1)){
+                        dp[i][j][k] = 1 + dp[i - 1][j - 1][k - 1];
+                    }else{
+                        dp[i][j][k] = Math.max(Math.max(dp[i - 1][j][k], dp[i][j - 1][k]), dp[i][j][k - 1]);
+                    }
+                }
+            }
+        }
+            
+        return dp[n1][n2][n3];
     }
 }
