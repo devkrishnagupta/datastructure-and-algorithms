5e24eba68bff6ba016743d65cdc220c466e523b9
Dev Krishna Gupta <dev.trisect@gmail.com>
2024-03-25 22:48:16 +0530

    Print N-bit binary numbers having more 1s than 0s

--- SpecificAlgorithmBased/Hare and Tortoise/287. Find the Duplicate Number/287. Find the Duplicate Number.java
+++ SpecificAlgorithmBased/Hare and Tortoise/287. Find the Duplicate Number/287. Find the Duplicate Number.java
@@ -1,4 +1,22 @@
-// Approach-1
+// Approach-1 (Two loop check for every element of one to another)
+// T.C : O(n * n)
+// S.C : O(1)
+
+// Approach-2 (Sorting and check consecutive)
+// T.C : O(n * logN)
+// S.C : O(1)
+
+
+// Approach-3 (HashMap sore and check the frequency is more than one of a particular element)
+// T.C : O(n)
+// S.C : O(n)
+
+// Approach-4 (Modifying the original Array)
+//(Go to element index & make them negative element if element is already negative then that mesan duplicate lement)
+// T.C : O(n)
+// S.C : O(1)
+
+// Approach-5 (Hare and Tortoise     ~   Two pointer approach)
 // T.C : O(n)
 // S.C : O(1)
 class Solution {
