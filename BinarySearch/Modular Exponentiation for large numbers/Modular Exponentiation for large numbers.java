522873b2d12f016da4e4f0cdf3f66971fd89e6ce
Dev Krishna Gupta <dev.trisect@gmail.com>
2024-05-20 23:45:06 +0530

    Modular Exponentiation for large numbers

---
+++ BinarySearch/Modular Exponentiation for large numbers/Modular Exponentiation for large numbers.java
@@ -0,0 +1,17 @@
+// Approach-1
+// T.C : O(logN)
+// S.C : O(1)
+class Solution
+{
+    public long PowMod(long x, long n, long m)
+    {
+        // Code here
+        long ans =1;
+	    while(n>0){
+	        if(n%2==1)ans=(ans*x)%m;
+	        x=(x*x)%m;
+	        n/=2;
+	    }
+	    return ans;
+    }
+}
