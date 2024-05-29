f9fb16202165fcafd7d092bd6e403c780b2a3e45
Dev Krishna Gupta <dev.trisect@gmail.com>
2024-05-29 23:55:57 +0530

    Geek and its Game of Coins

---
+++ DynamicProgramming/Geek and its Game of Coins/# Geek and its Game of Coins.cpp
@@ -0,0 +1,26 @@
+// Approach-1
+// T.C : O()
+// S.C : O()
+class Solution {
+  public:
+    int findWinner(int n, int x, int y) {
+        // code here
+        vector<int>arr(n+1,0);
+        arr[1]=1;
+        for(int i=2;i<=n;i++){
+            int j=i-1;
+            if(j>=0 && arr[j]==0){
+                arr[i]=1;
+            }
+            j=i-x;
+            if(j>=0 && arr[j]==0){
+                arr[i]=1;
+            }
+            j=i-y;
+            if(j>=0 && arr[j]==0){
+                arr[i]=1;
+            }
+        }
+        return arr[n];
+    }
+};
