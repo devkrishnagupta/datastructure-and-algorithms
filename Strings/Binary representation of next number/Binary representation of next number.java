6c729757ecb7548c9bd0c4751ae0fa23ae28ec9c
Dev Krishna Gupta <dev.trisect@gmail.com>
2024-06-04 12:01:56 +0530

    Binary representation of next number

---
+++ Strings/Binary representation of next number/Binary representation of next number.java
@@ -0,0 +1,29 @@
+// Approach-1
+// T.C : O(n)
+// S.C : O(n)   ~   to store the result only
+class Solution {
+    String binaryNextNumber(String s) {
+        // code here.
+        boolean indication=true;
+        char[] temp = s.toCharArray();
+        for(int i=s.length()-1;i>=0;i--){
+            if(temp[i]=='0'){
+                temp[i]='1';
+                indication=false;
+                break;
+            }
+            else temp[i]='0';
+        }
+        StringBuilder ans = new StringBuilder();
+        if(indication==true){
+            ans.append('1');
+            ans.append(temp);
+        }
+        else{
+            int index=0;
+            while(index<s.length() && temp[index]=='0')index++;
+            ans.append(temp,index,s.length()-index);
+        }
+        return ans.toString();
+    }
+}
