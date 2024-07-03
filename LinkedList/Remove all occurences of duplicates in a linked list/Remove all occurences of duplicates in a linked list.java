131e8c8201fecd3cc2948d8ad8e5219efbbac52d
Dev Krishna Gupta <dev.trisect@gmail.com>
2024-07-03 23:55:10 +0530

    Remove all occurences of duplicates in a linked list

---
+++ LinkedList/Remove all occurences of duplicates in a linked list/Remove all occurences of duplicates in a linked list.java
@@ -0,0 +1,27 @@
+// Approach-1
+// T.C : O(n)
+// S.C : O(1)
+class Solution {
+    public Node removeAllDuplicates(Node head) {
+        // code here
+        if(head.next==null)return head;
+        Node ans=new Node(-1),prev=head;
+        Node temp=ans;
+        head=head.next;
+        int count=1;
+        while(head!=null){
+            if(prev.data==head.data)count++;
+            else if(count==1){
+                temp.next=prev;
+                temp=temp.next;
+            }
+            else count=1;
+            prev=head;
+            head=head.next;
+        }
+        if(count==1)temp.next=prev;
+        else
+        temp.next=null;
+        return ans.next;
+    }
+}
