2533ab530fe39a11099715fcde42227e50a6693c
Dev Krishna Gupta <dev.trisect@gmail.com>
2024-07-21 22:44:11 +0530

    Maximum product subset of an array

---
+++ Subset/Maximum product subset of an array/Maximum product subset of an array.java
@@ -0,0 +1,28 @@
+// Approach-1 (Brute Force) (Find all subsets and get Maximum prodcuts)
+// T.C : O(2 ^ n)
+// S.C : O()
+
+// Approach-1
+// T.C : O(n)
+// S.C : O(1)
+class Solution {
+    public long findMaxProduct(List<Integer> arr) {
+        // code here
+        if(arr.size()==1)return arr.get(0);
+        long pos=1,neg=1,negLar=Long.MIN_VALUE,mod=1000000007,c1=0,c2=0;
+        for(Integer x:arr){
+            if(x<0){
+                neg=(neg*(long)x)%mod;;
+                negLar=Math.max((long)x,negLar);
+                c1++;
+            }
+            if(x>0){
+                pos=(pos*(long)x)%mod;
+                c2++;
+            }
+        }
+        if(c1<=1 && c2==0)return 0;
+        if(neg<0)neg/=negLar;
+        return (pos*neg)%mod;
+    }
+}
---
+++ Subset/README.md
@@ -0,0 +1,1 @@
+# Subset Questions Based on DSA Concepts!
