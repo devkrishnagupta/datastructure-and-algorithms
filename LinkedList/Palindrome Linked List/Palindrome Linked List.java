2dc7637d5845d7e7dcda3a6c79fd33d36755a184
Dev Krishna Gupta <dev.trisect@gmail.com>
2024-09-26 23:20:45 +0530

    Roof Top

--- LinkedList/Palindrome Linked List/Palindrome Linked List.java
+++ LinkedList/Palindrome Linked List/Palindrome Linked List.java
@@ -22,3 +22,45 @@ class Solution {
         return true;
     }
 }
+
+// Approach-2 (After reversing the second half)
+// T.C : O(n)
+// S.C : O(1)
+class Solution {
+    // Function to check whether the list is palindrome.
+    boolean isPalindrome(Node head) {
+        // Your code here
+        if(head == null || head.next == null){
+            return true;
+        }
+        
+        // Step 1. Find the middle element using two pointer
+        Node slow = head, fast = head;
+        while(fast != null && fast.next == null){
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+        
+        // Step 2. Reverse the second half
+        Node prev = null, curr = slow, next = null;
+        while(curr != null){
+            next = curr.next;
+            curr.next = prev;
+            prev = curr;
+            curr = next;
+        }
+        
+        // Step 3. Compare the first Half and reversed second half.
+        Node left = head;
+        Node right = prev;
+        while(left != null){
+            if(left.data != right.data){
+                return false;
+            }
+            left = left.next;
+            right = right.next;
+        }
+        
+        return true; // if all value matched
+    }
+}
---
+++ Searching/README.md
@@ -0,0 +1,1 @@
+# Searching Questions Based on DSA Concepts!
