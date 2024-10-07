8c8cc19693d7ca0c9c52bfd9fd5e444e23d5a103
Dev Krishna Gupta <dev.trisect@gmail.com>
2024-10-07 23:24:53 +0530

    XOR Linked List

---
+++ LinkedList/XOR Linked List/XOR Linked List.cpp
@@ -0,0 +1,23 @@
+// Approach-1
+// T.C : O()
+// S.C : O()
+struct Node *insert(struct Node *head, int data) {
+    // Code here
+    Node *temp = new Node(data);
+    temp->npx = head;
+    if(head)head->npx = XOR(temp,head->npx);
+    return temp;
+}
+
+vector<int> getList(struct Node *head) {
+    // Code here
+    vector<int> ans;
+    Node *curr=head,*prev=NULL,*nxt=NULL;
+    while(curr){
+        ans.push_back(curr->data);
+        nxt = XOR(prev,curr->npx);
+        prev=curr;
+        curr=nxt;
+    }
+    return ans;
+}
