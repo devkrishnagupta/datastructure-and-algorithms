f1830fe09f97ed380ab6e393a1ddb012af0d80c2
Dev Krishna Gupta <dev.trisect@gmail.com>
2024-12-06 23:58:16 +0530

    Find H-Index

---
+++ Arrays/Find H-Index/Find H-Index.java
@@ -0,0 +1,18 @@
+// Approach-1
+// T.C : O()
+// S.C : O()
+class Solution {
+    // Function to find hIndex
+    public int hIndex(int[] citations) {
+        // code here
+        Arrays.sort(citations);
+        int n = citations.length;
+        for(int i = 0; i < n; ++i){
+            if(citations[i] >= n - i){
+                return n - i;
+            }
+        }
+        return 0;
+        
+    }
+}
