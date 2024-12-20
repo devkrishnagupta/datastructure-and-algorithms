f15a8993803fc759e7d7755adeb7ae4d1387a57c
Dev Krishna Gupta <dev.trisect@gmail.com>
2024-12-20 12:18:21 +0530

    Spirally traversing a matrix

--- Matrix/Spirally traversing a matrix/Spirally traversing a matrix.java
+++ Matrix/Spirally traversing a matrix/Spirally traversing a matrix.java
@@ -22,3 +22,50 @@ class Solution {
         return ans;
     }
 }
+
+// Approach-2
+// T.C : O()
+// S.C : O()
+class Solution {
+    // Function to return a list of integers denoting spiral traversal of matrix.
+    public ArrayList<Integer> spirallyTraverse(int[][] mat) {
+        // Create an ArrayList to store the result
+        ArrayList<Integer> ans = new ArrayList<>();
+        
+        // Initialize the boundaries for traversal
+        int top = 0, left = 0, bottom = mat.length - 1, right = mat[0].length - 1;
+        
+        // Traverse the matrix in spiral order
+        while (top <= bottom && left <= right) {
+            // Traverse from left to right along the top row
+            for (int i = left; i <= right; i++) {
+                ans.add(mat[top][i]);
+            }
+            top++;
+            if (top > bottom) break;
+
+            // Traverse from top to bottom along the right column
+            for (int i = top; i <= bottom; i++) {
+                ans.add(mat[i][right]);
+            }
+            right--;
+            if (left > right) break;
+
+            // Traverse from right to left along the bottom row
+            for (int i = right; i >= left; i--) {
+                ans.add(mat[bottom][i]);
+            }
+            bottom--;
+            if (top > bottom) break;
+
+            // Traverse from bottom to top along the left column
+            for (int i = bottom; i >= top; i--) {
+                ans.add(mat[i][left]);
+            }
+            left++;
+        }
+        
+        // Return the result
+        return ans;
+    }
+}
