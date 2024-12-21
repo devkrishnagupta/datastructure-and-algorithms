4cd2f3de1b7cb00ea8be63b673de8e483b1171c6
Dev Krishna Gupta <dev.trisect@gmail.com>
2024-12-21 23:59:15 +0530

    Rotate by 90 degree

--- MatrixStandards/Rotate by 90 degree/Rotate by 90 degree.java
+++ MatrixStandards/Rotate by 90 degree/Rotate by 90 degree.java
@@ -21,3 +21,24 @@ class GFG {
         }
     }
 }
+
+// Approach-2
+// T.C : O(n * n)
+// S.C : O(1)
+class GFG {
+    static void rotateby90(int mat[][]) {
+        int n = mat.length;
+        
+        for(int i=0;i<n/2;i++){
+            for(int j=i;j<n-1-i;j++){
+                int temp = mat[i][j];
+                
+                mat[i][j] = mat[j][n-1-i];
+                mat[j][n-1-i] = mat[n-1-i][n-1-j];
+                mat[n-1-i][n-1-j] = mat[n-1-j][i];
+                mat[n-1-j][i]=temp;
+                
+            }
+        }
+    }
+}
