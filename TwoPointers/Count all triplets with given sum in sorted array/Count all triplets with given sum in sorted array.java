1b9a318d531b33d8997449ef1af24db05e917b8d
Dev Krishna Gupta <dev.trisect@gmail.com>
2025-01-04 12:32:47 +0530

    Count all triplets with given sum in sorted array

---
+++ TwoPointers/Count all triplets with given sum in sorted array/Count all triplets with given sum in sorted array.java
@@ -0,0 +1,27 @@
+// Approach-1
+// T.C : O()
+// S.C : O()
+class Solution {
+    public int countTriplets(int[] arr, int target) {
+        // Code Here
+        int count=0;
+        int n=arr.length;
+        for(int i=0;i<=n-3;i++){
+            int j=i+1,k=n-1;
+            while(j<k){
+                int sum=arr[i]+arr[j]+arr[k];
+                if(sum>target) k--;
+                else if(sum<target)j++;
+                else if(sum==target){
+                    count++;
+                    int temp=j+1;
+                    while(temp<k&& arr[temp]==arr[temp-1]){
+                        count++;temp++;
+                    };
+                    k--;
+                }
+            }
+        }
+        return count;
+    }
+}
