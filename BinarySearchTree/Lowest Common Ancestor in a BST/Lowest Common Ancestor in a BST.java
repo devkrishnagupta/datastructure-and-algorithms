2b980bb92f85c35f0b9d6da158a24caa1a7099f0
Dev Krishna Gupta <dev.trisect@gmail.com>
2025-02-15 12:33:46 +0530

    Lowest Common Ancestor in a BST

---
+++ BinarySearchTree/Lowest Common Ancestor in a BST/Lowest Common Ancestor in a BST.java
@@ -0,0 +1,15 @@
+// Approach-1
+// T.C : O()
+// S.C : O()
+class Solution {
+    static Node LCA(Node root, Node n1, Node n2) {
+        while (root != null) {
+            if (root.data > n1.data && root.data > n2.data) root = root.left;
+            else if (root.data < n1.data && root.data < n2.data)
+                root = root.right;
+            else
+                break;
+        }
+        return root;
+    }
+}
