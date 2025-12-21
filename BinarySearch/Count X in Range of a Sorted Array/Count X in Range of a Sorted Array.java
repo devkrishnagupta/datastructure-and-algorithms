789962e31ac4078d152572e46819bb294285a983
Dev Krishna Gupta <dev.trisect@gmail.com>
2025-12-21 23:35:44 +0530

    Count X in Range of a Sorted Array

---
+++ BinarySearch/Count X in Range of a Sorted Array/Count X in Range of a Sorted Array.java
@@ -0,0 +1,59 @@
+// Approach-1
+// T.C : O(n * n)
+// S.C : O(1)
+
+// Approach-2
+// T.C : O(q * logN)
+// S.C : O(1)
+class Solution {
+    public ArrayList<Integer> countXInRange(int[] arr, int[][] queries) {
+        ArrayList<Integer> ans = new ArrayList<>();
+
+        for (int[] q : queries) {
+            int l = q[0];
+            int r = q[1];
+            int x = q[2];
+
+            int first = firstOccurrence(arr, l, r, x);
+            if (first == -1) {
+                ans.add(0);
+            } else {
+                int last = lastOccurrence(arr, l, r, x);
+                ans.add(last - first + 1);
+            }
+        }
+        return ans;
+    }
+
+    private int firstOccurrence(int[] arr, int l, int r, int x) {
+        int res = -1;
+        while (l <= r) {
+            int mid = l + (r - l) / 2;
+            if (arr[mid] == x) {
+                res = mid;
+                r = mid - 1; // move left
+            } else if (arr[mid] < x) {
+                l = mid + 1;
+            } else {
+                r = mid - 1;
+            }
+        }
+        return res;
+    }
+
+    private int lastOccurrence(int[] arr, int l, int r, int x) {
+        int res = -1;
+        while (l <= r) {
+            int mid = l + (r - l) / 2;
+            if (arr[mid] == x) {
+                res = mid;
+                l = mid + 1; // move right
+            } else if (arr[mid] < x) {
+                l = mid + 1;
+            } else {
+                r = mid - 1;
+            }
+        }
+        return res;
+    }
+}
